a3d41edd56c7b7f26141cf4bbc6082b892bda754
DiegoC <dfer.cortes@gmail.com>
2017-12-20 07:39:54 -0500

    Add logic of news section page #21
    Created news section.

--- app/src/main/java/org/sabio/sabioapp/presentation/view/fragment/NewsFragment.java
+++ app/src/main/java/org/sabio/sabioapp/presentation/view/fragment/NewsFragment.java
@@ -3,9 +3,21 @@ package org.sabio.sabioapp.presentation.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.TimelineResult;
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import org.sabio.sabioapp.R;
 
@@ -15,6 +27,7 @@ import org.sabio.sabioapp.R;
  */
 public class NewsFragment extends BaseFragment {
 
+    private RecyclerView rvNewsSabio;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -28,7 +41,25 @@ public class NewsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+         rvNewsSabio = view.findViewById(R.id.rvNewsSabios);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvNewsSabio.setLayoutManager(layoutManager);
+
+        UserTimeline userTimeline = new UserTimeline.Builder().screenName("SABIOfutbol").build();
+
+        TweetTimelineRecyclerViewAdapter adapter =
+                new TweetTimelineRecyclerViewAdapter.Builder(getContext())
+                        .setTimeline(userTimeline)
+                        .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+                        .build();
+
+        rvNewsSabio.setAdapter(adapter);
+
+        return view;
     }
 
 }
